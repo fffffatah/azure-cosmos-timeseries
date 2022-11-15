a7e63bfdc860ef2b2038bce43851e025e807ad50
fffffatah <ab.fatahmn@gmail.com>
2022-11-15 23:29:58 +0600

    update gitignore #2

---
+++ src/Azure.Cosmos.Timeseries/Azure.Cosmos.Timeseries/Core/Models/Tub/Tub.cs
@@ -0,0 +1,12 @@
+﻿namespace Azure.Cosmos.Timeseries.Core.Models.Tub
+{
+    public class Tub : BaseContainerModel
+    {
+        public string SensorId { get; set; }
+        public string Type { get; set; }
+        public double[] MeasuredAts { get; set; }
+        public double[] Measurements { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
